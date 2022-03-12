5e08a228014053592a9c64385fa3ae400bbb4d0f
Camilla Franceschini <camilla.franceschini97@gmail.com>
2022-03-12 11:35:52 +0100

    test API

--- routes/auth.js
+++ routes/auth.js
@@ -3,11 +3,33 @@ const User = require("../models/User");
 const CryptoJS = require("crypto-js");
 const jwt = require("jsonwebtoken");
 
+/*
+//REGISTER
+router.post("/register", async (req, res) => {
+
+  const newUser = new User({
+    username: req.body.username,
+    email:    req.body.email,
+    password: req.body.password,
+  });
+
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+    console.log(savedUser);
+  } catch(err) {
+    res.status(500).json(err);
+    console.log(err);
+  }
+});
+*/
+
+
 //REGISTER
 router.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
-    email: req.body.email,
+    email:    req.body.email,
     password: CryptoJS.AES.encrypt(
       req.body.password,
       process.env.PASS_SEC
@@ -22,6 +44,41 @@ router.post("/register", async (req, res) => {
   }
 });
 
+//LOGIN
+router.post("/login", async (req, res) => {
+  try{
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(401).json("Wrong username!")
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password, 
+      process.env.PASS_SEC
+      );
+    const Originalpassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+    Originalpassword !== req.body.password && 
+      res.status(401).json("Wrong password!");
+
+      const accessToken = jwt.sign(
+      {
+        id: user._id, 
+        isAdmin: user.idAdmin,
+      }, 
+      process.env.JWT_SEC,
+       {expiresIn:"3d"}
+      );
+
+      const { password, ...others } = user._doc;
+
+      res.status(200).json({...others, accessToken});
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
+/*
 //LOGIN
 
 router.post('/login', async (req, res) => {
@@ -64,5 +121,6 @@ router.post('/login', async (req, res) => {
     }
 
 });
+*/
 
 module.exports = router;
